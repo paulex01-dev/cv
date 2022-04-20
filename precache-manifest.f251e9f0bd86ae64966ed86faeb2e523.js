72baa6c2a9243d25c16ea21c06e4b53b7d939a6e
Oladehin Olamigoke <oladehinolamigoke@gmail.com>
2022-04-21 00:30:45 +0100

    Updates

---
+++ precache-manifest.f251e9f0bd86ae64966ed86faeb2e523.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "94dd250e4140e5cad7a1",
+    "url": "/cv/static/css/main.bbe71aa0.chunk.css"
+  },
+  {
+    "revision": "94dd250e4140e5cad7a1",
+    "url": "/cv/static/js/main.94dd250e.chunk.js"
+  },
+  {
+    "revision": "dd67395527e48a26f7d1",
+    "url": "/cv/static/js/1.dd673955.chunk.js"
+  },
+  {
+    "revision": "084ac45aee39707f976e",
+    "url": "/cv/static/js/runtime~main.084ac45a.js"
+  },
+  {
+    "revision": "75e8a7b948e199e20f410e20c343c028",
+    "url": "/cv/index.html"
+  }
+];
